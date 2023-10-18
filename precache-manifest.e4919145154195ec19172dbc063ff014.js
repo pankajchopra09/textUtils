becfa6e49145964725437258eab61952af3e7463
pankajchopra09 <pankajchopra09@gmail.com>
2023-10-18 20:30:48 +0530

    Updates

---
+++ precache-manifest.e4919145154195ec19172dbc063ff014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab833d6df6ef47badf4f4bcde87c4284",
+    "url": "/textUtils/index.html"
+  },
+  {
+    "revision": "570f8cb478b7d6c9ab9e",
+    "url": "/textUtils/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bf5c34c705ad50e3c406",
+    "url": "/textUtils/static/js/2.af3fc02c.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/textUtils/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "570f8cb478b7d6c9ab9e",
+    "url": "/textUtils/static/js/main.0bb09a55.chunk.js"
+  },
+  {
+    "revision": "03883961fdbb7d1379a1",
+    "url": "/textUtils/static/js/runtime~main.f6e5cba1.js"
+  }
+]);
